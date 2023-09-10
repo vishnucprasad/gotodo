084173deee21b52e8bc0e50f04d579bd732ea034
vishnucprasad <mail@vishnucprasad.in>
2023-09-10 12:52:18 +0530

    Designed bottomsheet for create todo form

--- lib/presentation/widgets/gotodo_app_bar.dart
+++ lib/presentation/widgets/gotodo_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gotodo/presentation/core/colors.dart';
 import 'package:gotodo/presentation/core/constants.dart';
 
 class GotodoAppBar extends StatelessWidget {
@@ -8,6 +9,9 @@ class GotodoAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode =
+        Theme.of(context).colorScheme.brightness == Brightness.dark;
+
     return Row(
       children: [
         kWidth,
@@ -21,9 +25,13 @@ class GotodoAppBar extends StatelessWidget {
           ),
         ),
         const Spacer(),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.person),
+        CircleAvatar(
+          backgroundColor:
+              isDarkMode ? secondaryDarkColor : secondaryLightColor,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person),
+          ),
         ),
         kWidth,
       ],
