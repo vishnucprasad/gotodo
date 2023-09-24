8124e4587d624e0238c163702ad0e75bf48b3d61
vishnucprasad <mail@vishnucprasad.in>
2023-09-24 22:44:55 +0530

    Modified API base URL

--- lib/domain/core/constants.dart
+++ lib/domain/core/constants.dart
@@ -1,5 +1,5 @@
 // const String kApiBaseUrl = "http://10.0.2.2:3000";
-const String kApiBaseUrl = "https://gotodo-un7v.onrender.com";
+const String kApiBaseUrl = "https://gotodo.cyclic.cloud";
 
 enum TokenType {
   accessToken,
